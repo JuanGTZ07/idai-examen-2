b77a9836b4fca694de48a8b5f3b0326dcb3e9c7e
JuanGTZ07 <al45677@un.edu.mx>
2025-10-28 09:34:50 -0600

    js

--- javascript.js
+++ javascript.js
@@ -7,5 +7,21 @@ var minutos = formulario.MinLlam.value;
 total = minutos * 10;
 var cliente = formulario.cliente.value;
 alert(cliente);
+
+if (formulario.plan.value == "ST") {
+    //entra aqui porque es verdadera
+    total = total + 200;
+}
+
+if (formulario.plan.value == "TI") {
+    //entra aqui porque es verdadera
+    total = total + 450;
+}
+
+if (formulario.plan.value == "TITV") {
+    //entra aqui porque es verdadera
+    total = total + 850;
+}
+alert(total);
 }
 
