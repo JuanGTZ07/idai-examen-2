55b4246bb4d41cb13efa947500bc4ab066233eaa
JuanGTZ07 <al45677@un.edu.mx>
2025-10-28 09:19:41 -0600

    .

---
+++ javascript.js
@@ -0,0 +1,11 @@
+var formulario;
+formulario = document.getElementById("calculoMinutos");
+
+function calcular() {
+var total = 0;
+var minutos = formulario.MinLlam.value;
+total = minutos * 10;
+var cliente = formulario.cliente.value;
+alert(cliente);
+}
+
